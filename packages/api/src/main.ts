12e985fd67e3e5bd426a9acb1e89d58da163fb6c
Bratu Sebastian <artweb11@gmail.com>
2023-07-23 23:32:19 +0300

    update port2

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
